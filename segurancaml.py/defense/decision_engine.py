6be41a0609a00d6e9407b4918a06592bbb440a9f
Luiz Fernando Araujo Santos <luizfernandotec.link2@gmail.com>
2026-03-17 21:46:33 -0300

    Delete segurancaml.py directory

--- segurancaml.py/defense/decision_engine.py
+++
@@ -1,7 +0,0 @@
-def decide(prediction, probability):
-    if prediction == 1 and probability > 0.8:
-        return "BLOCK"
-    elif prediction == 1:
-        return "ALERT"
-    else:
-        return "ALLOW"
